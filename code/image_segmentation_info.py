4a01a7178059ef798187257f4027afcb56b57ac7
Alexander Fache <alexander.fache@gmail.com>
2020-10-02 13:55:57 -0400

    image alignment

--- code/image_segmentation_info.py
+++ code/image_segmentation_info.py
@@ -5,19 +5,17 @@ BORDER_W = 50
 BORDER_H = 50
 
 PRECROP = True
-SAVE_BORDERED_IMAGE = True
-
 SELECT_REGIONS = True
 SELECT_SAMPLES = True
 SELECT_LANDMARKS = True
 
-SAVE_REGIONS = True
-SAVE_SAMPLES = True
-SAVE_LANDMARKS = True
+SAVE_BORDERED_IMAGE = False
+SAVE_REGIONS = False
+SAVE_SAMPLES = False
+SAVE_LANDMARKS = False
 
 def get_mission_file_path(mission_number):
     return '..\\missions\\mission_' + str(mission_number) + '\\mission_' + str(mission_number) + '_'
-
 
 def get_mission_segmentation_file_path(mission_number):
     return '..\\missions\\mission_' + str(mission_number) + '_segmentation\\mission_' + str(mission_number) + '_'
