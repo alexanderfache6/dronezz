96fd24584cb12a4e9dede67b5df710f86c2280c1
Alexander Fache <alexander.fache@gmail.com>
2020-10-19 23:11:57 -0400

    testing bucket segmentation for dark green to dark brown, issue with bucket coverage

--- code/hsv_finder.py
+++ code/hsv_finder.py
@@ -12,7 +12,7 @@ def nothing(x):
 def get_mission_file_path(mission_number):
     return '..\\missions\\mission_' + str(mission_number) + '\\mission_' + str(mission_number) + '_'
 
-def run(mission_number, using_video=False):
+def run(mission_number, use_BGR, using_video=False):
 
     if using_video:
         capture = cv2.VideoCapture(0)
@@ -22,20 +22,29 @@ def run(mission_number, using_video=False):
     cv2.namedWindow("Trackbar")
     cv2.resizeWindow("Trackbar", 1000, 400);
 
-    # Now create 6 trackbars that will control the lower and upper range of
-    # H,S and V channels. The Arguments are like this: Name of trackbar,
-    # window name, range,callback function. For Hue the range is 0-179 and
-    # for S,V its 0-255.
-    cv2.createTrackbar("H lower", "Trackbar", 0, 179, nothing)
-    cv2.createTrackbar("S lower", "Trackbar", 0, 255, nothing)
-    cv2.createTrackbar("V lower", "Trackbar", 0, 255, nothing)
-    cv2.createTrackbar("H upper", "Trackbar", 179, 179, nothing)
-    cv2.createTrackbar("S upper", "Trackbar", 255, 255, nothing)
-    cv2.createTrackbar("V upper", "Trackbar", 255, 255, nothing)
+    if use_BGR:
+        cv2.createTrackbar("B lower", "Trackbar", 0, 255, nothing)
+        cv2.createTrackbar("G lower", "Trackbar", 0, 255, nothing)
+        cv2.createTrackbar("R lower", "Trackbar", 0, 255, nothing)
+        cv2.createTrackbar("B upper", "Trackbar", 255, 255, nothing)
+        cv2.createTrackbar("G upper", "Trackbar", 255, 255, nothing)
+        cv2.createTrackbar("R upper", "Trackbar", 255, 255, nothing)
+    else:
+        cv2.createTrackbar("H lower", "Trackbar", 0, 179, nothing)
+        cv2.createTrackbar("S lower", "Trackbar", 0, 255, nothing)
+        cv2.createTrackbar("V lower", "Trackbar", 0, 255, nothing)
+        cv2.createTrackbar("H upper", "Trackbar", 179, 179, nothing)
+        cv2.createTrackbar("S upper", "Trackbar", 255, 255, nothing)
+        cv2.createTrackbar("V upper", "Trackbar", 255, 255, nothing)
 
     if not using_video:
-        frame = np.load(get_mission_file_path(mission_number) + 'aligned_image.npy')
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # frame = np.load(get_mission_file_path(mission_number) + 'aligned_image.npy')
+        # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        if use_BGR:
+            frame = np.load(get_mission_file_path(mission_number) + 'histogram_matched_image.npy')
+        else:
+            frame = np.load(get_mission_file_path(mission_number) + 'histogram_matched_image_hsv.npy')
 
     while True:
 
@@ -46,24 +55,29 @@ def run(mission_number, using_video=False):
             frame = cv2.flip(frame, 1)
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        l_h = cv2.getTrackbarPos("H lower", "Trackbar")
-        l_s = cv2.getTrackbarPos("S lower", "Trackbar")
-        l_v = cv2.getTrackbarPos("V lower", "Trackbar")
-        u_h = cv2.getTrackbarPos("H upper", "Trackbar")
-        u_s = cv2.getTrackbarPos("S upper", "Trackbar")
-        u_v = cv2.getTrackbarPos("V upper", "Trackbar")
+        if use_BGR:
+            l_1 = cv2.getTrackbarPos("B lower", "Trackbar")
+            l_2 = cv2.getTrackbarPos("G lower", "Trackbar")
+            l_3 = cv2.getTrackbarPos("R lower", "Trackbar")
+            u_1 = cv2.getTrackbarPos("B upper", "Trackbar")
+            u_2 = cv2.getTrackbarPos("G upper", "Trackbar")
+            u_3 = cv2.getTrackbarPos("R upper", "Trackbar")
+        else:
+            l_1 = cv2.getTrackbarPos("H lower", "Trackbar")
+            l_2 = cv2.getTrackbarPos("S lower", "Trackbar")
+            l_3 = cv2.getTrackbarPos("V lower", "Trackbar")
+            u_1 = cv2.getTrackbarPos("H upper", "Trackbar")
+            u_2 = cv2.getTrackbarPos("S upper", "Trackbar")
+            u_3 = cv2.getTrackbarPos("V upper", "Trackbar")
 
-        lower_range = np.array([l_h, l_s, l_v])
-        upper_range = np.array([u_h, u_s, u_v])
+        lower_range = np.array([l_1, l_2, l_3])
+        upper_range = np.array([u_1, u_2, u_3])
 
-        mask = cv2.inRange(hsv, lower_range, upper_range)
-
+        mask = cv2.inRange(frame, lower_range, upper_range)
         masked_frame = cv2.bitwise_and(frame, frame, mask=mask)
-        masked_hsv = cv2.bitwise_and(hsv, hsv, mask=mask)
-
         mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) # so it can be stacked
 
-        stacked = np.hstack((frame, hsv, mask_3, masked_frame, masked_hsv))
+        stacked = np.hstack((frame, mask_3, masked_frame))
         cv2.imshow('Trackbars', cv2.resize(stacked, None, fx=0.8, fy=0.8))
 
         key = cv2.waitKey(1)
@@ -71,8 +85,8 @@ def run(mission_number, using_video=False):
             break
 
         if key == ord('s'):
-            print('lower:', [l_h, l_s, l_v])
-            print('upper:', [u_h, u_s, u_v])
+            print('lower:', [l_1, l_2, l_3])
+            print('upper:', [u_1, u_2, u_3])
 
             # np.save('hsv_limits', limits)
             break
@@ -82,10 +96,11 @@ def run(mission_number, using_video=False):
         cv2.destroyAllWindows()
 
 if __name__=='__main__':
-    if len(sys.argv) != 2:
-        sys.exit('[Error] Include Mission #')
+    if len(sys.argv) != 3:
+        sys.exit('[Error] Include Mission # & BGR')
     else:
         mission_number = int(sys.argv[1])
+        use_BGR = bool(sys.argv[1])
         print('Mission #%d' % (mission_number))
 
-    run(mission_number=mission_number, using_video=False)
+    run(mission_number=mission_number, use_BGR=use_BGR, using_video=False)
