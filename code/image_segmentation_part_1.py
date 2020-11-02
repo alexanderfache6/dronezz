3f34ac013492da35669abbe7c75b68c36e9886d3
Alexander Fache <alexander.fache@gmail.com>
2020-11-01 22:10:57 -0500

    able to import other functions

--- code/image_segmentation_part_1.py
+++ code/image_segmentation_part_1.py
@@ -6,7 +6,29 @@ import numpy as np
 import os
 import sys
 
-from image_segmentation_info import *
+# ----------------------------------------
+
+REGION_NAMES = ['GRASS', 'SIDEWALK', 'BUILDING', 'GRAVEL']
+
+BORDER = 100
+
+PRECROP = True
+SELECT_REGIONS = True
+SELECT_SAMPLES = False
+SELECT_LANDMARKS = False
+
+SAVE_BORDERED_IMAGE = True
+SAVE_REGIONS = False
+SAVE_SAMPLES = False
+SAVE_LANDMARKS = False
+
+def get_mission_file_path(mission_number):
+    return '..\\missions\\mission_' + str(mission_number) + '\\mission_' + str(mission_number) + '_'
+
+def get_mission_segmentation_file_path(mission_number):
+    return '..\\missions\\mission_' + str(mission_number) + '_segmentation\\mission_' + str(mission_number) + '_'
+
+# ----------------------------------------
 
 class ImageSegmentor():
 
